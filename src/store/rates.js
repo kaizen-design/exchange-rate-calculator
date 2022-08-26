6217f49651f19008fd02bfd3b353ef63e80c8a75
denis <denis@kaizen-design.ru>
2022-08-26 18:00:58 +0400

    Refactor to use Action Type Constants and Action Creators when Dispatching Redux Actions

--- src/store/rates.js
+++ src/store/rates.js
@@ -5,9 +5,9 @@ const initialState = {
 
 export default function ratesReducer(state = initialState, action) {
   switch (action.type) {
-    case 'rates/amountChanged':
+    case AMOUNT_CHANGED:
       return { ...state, amount: action.payload }
-    case 'rates/currencyCodeChanged':
+    case CURRENCY_CODE_CHANGED:
       return { ...state, currencyCode: action.payload }  
     default:
       return state;  
@@ -17,3 +17,17 @@ export default function ratesReducer(state = initialState, action) {
 //  Selectors
 export const getAmount = (state) => state.rates.amount;
 export const getCurrencyCode = (state) => state.rates.currencyCode;
+
+//  Action types
+export const AMOUNT_CHANGED = 'rates/amountChanged';
+export const CURRENCY_CODE_CHANGED = 'rates/currencyCodeChanged';
+
+//  Action creators
+export const changeAmount = (amount) => ({
+  type: AMOUNT_CHANGED,
+  payload: amount
+})
+export const changeCurrencyCode = (code) => ({
+  type: CURRENCY_CODE_CHANGED,
+  payload: code
+})
